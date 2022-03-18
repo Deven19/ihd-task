198e2b6eda9bf9b9137584daad43f7e1683abbb8
Devendra <devendra103@gmail.com>
2022-03-17 20:19:48 -0400

    Design Structure

--- app.js
+++ app.js
@@ -2,23 +2,8 @@ const express = require('express');
 const app = express();
 const configRoutes = require('./routes');
 const process = require('process');
-const sql = require('mssql')
-// const dataObj = require('./data')
-
-// app.use(session({
-//     name: 'AuthCookie',
-//     secret: '%rt8322f8AAAi5n-sdkjhfrtyyyyyyy',
-//     resave: false,
-//     saveUninitialized: true
-// }));
-
-// app.use('/public', static);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.set('views', path.join(__dirname, 'views'));
-
-// app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-// app.set('view engine', 'handlebars');
 
 configRoutes(app);
 
@@ -31,30 +16,7 @@ process.on('rejectionHandled', (promise) => {
 });
 
 
-
-const sqlConfig = {
-    user: 'ihd_db_admin', //process.env.DB_USER,
-    password: 'Admin@IHD1!', //process.env.DB_PWD,
-    database: 'IHD', //process.env.DB_NAME,
-    server: 'DESKTOP-P0VD5MJ',
-    trustServerCertificate: true,
-}
-
-
-
-
 app.listen(3000, async () => {
-   // async () => {
-        try {
-         // make sure that any items are correctly URL encoded in the connection string
-         await sql.connect(sqlConfig)
-         const result = await sql.query`SELECT * FROM IHD.DBO.VISITS`
-         console.log(result)
-        } catch (err) {
-            console.log(err)
-         // ... error checks
-        }
-//}
     console.log("Server is started now!");
     console.log('Your routes will be running on http://localhost:3000');
 });
